3f78c9ef2dc33fc41a969a1610ddcdf85d53d0bd
thewayfairer <rgutierrez428@mycr.redwoods.edu>
2023-03-01 18:44:11 -0800

    Completed

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -23,6 +23,12 @@ public class TempConversion {
         return kelvin - 273.15;
     }
 
+    public static double convertK2F(double kelvin) {
+        double celsius = convertK2C(kelvin);
+        double fahrenheit = convertC2F(celsius);
+        return fahrenheit;
+    }
+
     public static double getTemp(String unit) {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
@@ -38,7 +44,69 @@ public class TempConversion {
 
     public static void main(String[] args) {
         while(true) {
+            System.out.println("Choose the temperature unit to convert from:");
+            String fromUnit = getUnitChoice();
+            if(fromUnit.equals("Q")) {
+                break;
+            }
 
+            System.out.println("Choose the temperature unit to convert to:");
+            String toUnit = getUnitChoice();
+            if(toUnit.equals("Q")) {
+                break;
+            }
+
+            System.out.println("Enter the temperature value:");
+            double tempValue = Double.parseDouble(input.nextLine());
+
+            double result = 0.0;
+
+            switch(fromUnit) {
+                case "C":
+                    switch(toUnit) {
+                        case "F":
+                            result = convertC2F(tempValue);
+                            break;
+                        case "K":
+                            result = convertC2K(tempValue);
+                            break;
+                        default:
+                            System.out.println("Invalid choice!");
+                            break;
+                    }
+                    break;
+                case "F":
+                    switch(toUnit) {
+                        case "C":
+                            result = convertF2C(tempValue);
+                            break;
+                        case "K":
+                            result = convertF2K(tempValue);
+                            break;
+                        default:
+                            System.out.println("Invalid choice!");
+                            break;
+                    }
+                    break;
+                case "K":
+                    switch(toUnit) {
+                        case "C":
+                            result = convertK2C(tempValue);
+                            break;
+                        case "F":
+                            result = convertK2F(tempValue);
+                            break;
+                        default:
+                            System.out.println("Invalid choice!");
+                            break;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+
+            System.out.printf("Result: %.6f°%s is %.6f°%s%n", tempValue, fromUnit, result, toUnit);
         }
     }
 }
